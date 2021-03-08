258dcaace2bb9796d177a0c2d7bd785ba9f66421
suneel <suneel@dispatchtrack.com>
2021-03-08 14:57:47 +0530

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(name: 'test user', email: 'test@comments.com', password: 'testing', password_confirmation: 'testing')
+Post.create(title: "International Women's Day: Learnings for women from COVID pandemic
+", body: "Alisha Moopen, Deputy MD, Aster DM Healthcare, explains what COVID-19 pandemic has taught women. She lists the number of measures women should take to keep themselves healthy and also touches upon the sacrifices they had to make during the pandemic. Moopen describes how coronavirus increased the gender gap predicted by the World Economic Forum and steps required to lessen this gap. Watch the video for more.", user_id: 1)
+Comment.create(body: 'Happy Womens day', post_id: 1, user_id: 1)
+Comment.create(body: 'Happy Womens day! Have a wonderfull day', post_id: 1, user_id: 1)
+Comment.create(body: 'Mom, i love you.Happy Womens day', post_id: 1, user_id: 1)
